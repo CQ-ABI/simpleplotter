c61d8eced432d16225a96eb8be8aa34ab9451ae0
Wojtek Dabrowski <piotr.dabrowski@htw-berlin.de>
2022-02-07 21:07:25 +0100

    Added correct file (.py instead of .png)

--- plot.py
+++ plot.py
@@ -1,4 +1,25 @@
-f = open("data/example.csv")
+import matplotlib.pyplot as plt
+import numpy as np
+
+f = open("../data/example.csv")
 lines = [x.strip().split(",") for x in f.readlines()[1:]]
-print(len(lines))
 f.close()
+
+what = lines[0][1]
+how = lines[0][11]
+why = lines[0][9]
+where = lines[0][3]
+
+data = [(x[12], x[13]) for x in lines]
+data.sort(key = lambda x: x[0])
+
+years = [x[0] for x in data]
+vals = [float(x[1]) for x in data]
+print(vals)
+fig, ax = plt.subplots()
+ax.plot(years, vals)
+ax.set(xlabel="Year", ylabel=what + " (" + how + ")", title=what + " (" + how + ") due to " + why + " in " + where)
+ax.set_xticklabels(labels=years, rotation=45)
+fig.set_figwidth(10)
+fig.set_figheight(10)
+fig.savefig("plot.png")
